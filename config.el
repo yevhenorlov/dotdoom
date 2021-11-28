8ab9a0668ce2a3fc4997db5aabaad6e5af64a532
yevhen orlov <yevhenorlov@gmail.com>
2021-11-28 17:29:50 +0200

    add cider

--- config.el
+++ config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Yevhen Orlov"
+      user-mail-address "yevhenorlov@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -27,7 +27,8 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 ;; (setq doom-theme 'doom-one-light)
-(setq doom-theme 'doom-one)
+;; (setq doom-theme 'doom-one)
+   (setq doom-theme 'doom-dracula)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
